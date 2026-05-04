16830af7ab1f286e629a91bd872bbaf7ea9dbafa
jerryedensjr <ceciljedensjr@gmail.com>
2026-05-04 09:24:40 -0600

    Stop tracking auth-confgi.js

--- program_landings/auth-config.js
+++
@@ -1,6 +0,0 @@
-window.HALT_AUTH_CONFIG = Object.freeze({
-  cognitoRegion: "us-gov-west-1",
-  userPoolClient: "54sse43leqcns8reuvjmokpcjq",
-  intakeApiUrl: "https://r71pb7ej1d.execute-api.us-gov-west-1.amazonaws.com/program-intake",
-  apiBaseUrl: "https://r71pb7ej1d.execute-api.us-gov-west-1.amazonaws.com",
-});
